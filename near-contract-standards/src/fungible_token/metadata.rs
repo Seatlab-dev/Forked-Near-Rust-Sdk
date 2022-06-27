0544cca0567ac71ea87163f21d3b8c71438f4af5
Thiago Machado <thiago@keyko.io>
2022-06-27 15:26:30 -0300

    doc: improve the docs for some structures.
    - For non-wasm targets, preserves the json fields order.
        - This is useful for examples in the documentation.
    - Improve the docs and add a few examples to some data structures.

--- near-contract-standards/src/fungible_token/metadata.rs
+++ near-contract-standards/src/fungible_token/metadata.rs
@@ -13,22 +13,49 @@ pub const FT_METADATA_SPEC: &str = "ft-1.0.0";
 #[derive(BorshDeserialize, BorshSerialize, Clone, Deserialize, Serialize, JsonSchema)]
 #[serde(crate = "near_sdk::serde")]
 #[schemars(crate = "near_sdk::schemars")]
+#[schemars(example = "fungible_token_metadata_example")]
 pub struct FungibleTokenMetadata {
     /// Spec style and version.
     ///
+    /// Should be ft-1.0.0 to indicate that a Fungible Token contract adheres to the
+    /// current versions of this Metadata and the
+    /// [Fungible Token Core](https://nomicon.io/Standards/Tokens/FungibleToken/Core)
+    /// specs.
+    /// This will allow consumers of the Fungible Token to know if they support the
+    /// features of a given contract.
+    ///
     /// The standard validation for this structure requires the value "ft-1.0.0".
     pub spec: String,
+
     /// The human-readable name of the token.
     pub name: String,
+
     /// The abbreviation, like "wETH" or "AMPL".
     pub symbol: String,
+
     /// A small image associated with this token.
     ///
-    /// Must be a data URL, to help consumers display it quickly while protecting
-    /// user data. For a "square" symbol, one can use the URL-escaped version of
+    /// Must be a
+    /// [data URL](https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URLs),
+    /// to help consumers display it quickly while protecting user data.
+    ///
+    /// Recommendation: use optimized SVG, which can result in high-resolution
+    /// images with only 100s of bytes of
+    /// [storage cost](https://docs.near.org/docs/concepts/storage-staking).
+    /// (Note that these storage costs are incurred to the token owner/deployer, but
+    /// that querying these icons is a very cheap & cacheable read operation for all
+    /// consumers of the contract and the RPC nodes that serve the data.)
+    ///
+    /// Recommendation: create icons that will work well with both light-mode and
+    /// dark-mode websites by either using middle-tone color schemes, or by
+    /// [embedding media queries in the SVG](https://timkadlec.com/2013/04/media-queries-within-svg/).
+    ///
+    /// For a "square" symbol, one can use the URL-escaped version of
     /// `data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg'><rect width='50' height='50' /></svg>`.
     pub icon: Option<String>,
-    /// A link to a valid JSON file containing various keys offering supplementary details on the token.  
+
+    /// A link to a valid JSON file containing various keys offering supplementary
+    /// details on the token.  
     ///
     /// Eg:
     /// - `/ipfs/QmdmQXB2mzChmMeKY47C43LxUdg1NDJ5MWcKMKxDu7RgQm`.
@@ -37,13 +64,32 @@ pub struct FungibleTokenMetadata {
     /// If the information given in this document conflicts with the on-chain
     /// attributes, the values in reference shall be considered the source of truth.
     pub reference: Option<String>,
-    /// The base64-encoded sha256 hash of the JSON file contained in the reference
+
+    /// The base64-encoded sha256 hash of the JSON file contained in the `reference`
     /// field. This is to guard against off-chain tampering.
     pub reference_hash: Option<Base64VecU8>,
+
     /// The amount of decimals used by the token unit. Used in frontends to show the
-    /// proper significant digits of a token. This concept is explained well in this
+    /// proper significant digits of a token.
+    ///
+    /// This concept is explained well in this
     /// [OpenZeppelin post](https://docs.openzeppelin.com/contracts/3.x/erc20#a-note-on-decimals).
+    #[schemars(range(min = 0, max = 39))]
     pub decimals: u8,
+}
+
+pub fn fungible_token_metadata_example() -> FungibleTokenMetadata {
+    FungibleTokenMetadata {
+        spec: FT_METADATA_SPEC.into(),
+        name: "FT Example".into(),
+        symbol: "xFT".into(),
+        icon: Some("data%3Aimage%2Fsvg%2Bxml%2C%3Csvg%20xmlns%3D%27http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%27%3E%3Crect%20width%3D%2750%27%20height%3D%2750%27%20%2F%3E%3C%2Fsvg%3E".into()),
+        reference: Some("https://example.com/token.json".into()),
+        // from the hex:
+        // 00112233445566778899AABBCCDDEEFF0112233445566778899AABBCCDDEEFF0
+        reference_hash: Some(b"ABEiM0RVZneImaq7zN3u/wESIzRFVmd4iZqrvM3e7/A=".to_vec().into()),
+        decimals: 8,
+    }
 }
 
 pub trait FungibleTokenMetadataProvider {
--- near-contract-standards/src/non_fungible_token/metadata.rs
+++ near-contract-standards/src/non_fungible_token/metadata.rs
@@ -8,40 +8,175 @@ use near_sdk::serde::{Deserialize, Serialize};
 pub const NFT_METADATA_SPEC: &str = "nft-1.0.0";
 
 /// Metadata for the NFT contract itself.
+///
+/// See [NEP-177](https://nomicon.io/Standards/Tokens/NonFungibleToken/Metadata) for
+/// more info.
 #[derive(
     BorshDeserialize, BorshSerialize, Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema,
 )]
 #[serde(crate = "near_sdk::serde")]
 #[schemars(crate = "near_sdk::schemars")]
+#[schemars(example = "nft_contract_metadata_example")]
 pub struct NFTContractMetadata {
-    pub spec: String,              // required, essentially a version like "nft-1.0.0"
-    pub name: String,              // required, ex. "Mosaics"
-    pub symbol: String,            // required, ex. "MOSIAC"
-    pub icon: Option<String>,      // Data URL
-    pub base_uri: Option<String>, // Centralized gateway known to have reliable access to decentralized storage assets referenced by `reference` or `media` URLs
-    pub reference: Option<String>, // URL to a JSON file with more info
-    pub reference_hash: Option<Base64VecU8>, // Base64-encoded sha256 hash of JSON from reference field. Required if `reference` is included.
+    /// The Metadata spec version.
+    ///
+    /// This will allow consumers of the Non-Fungible Token to know which set of
+    /// metadata features the contract supports.
+    ///
+    /// Eg. "nft-1.0.0".
+    #[schemars(regex(pattern = r"^nft-\d+\.\d+\.\d+$"))]
+    pub spec: String,
+
+    /// The human-readable name of the contract.
+    ///
+    /// Eg. "Mochi Rising — Digital Edition", "Metaverse 3".
+    pub name: String,
+
+    /// The abbreviated symbol of the contract.
+    ///
+    /// Eg. "MOCHI", "MV3".
+    pub symbol: String,
+
+    /// A small image associated with this token.
+    ///
+    /// Must be a
+    /// [data URL](https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URLs),
+    /// to help consumers display it quickly while protecting user data.
+    ///
+    /// Recommendation: use optimized SVG, which can result in high-resolution
+    /// images with only 100s of bytes of
+    /// [storage cost](https://docs.near.org/docs/concepts/storage-staking).
+    /// (Note that these storage costs are incurred to the token owner/deployer, but
+    /// that querying these icons is a very cheap & cacheable read operation for all
+    /// consumers of the contract and the RPC nodes that serve the data.)
+    ///
+    /// Recommendation: create icons that will work well with both light-mode and
+    /// dark-mode websites by either using middle-tone color schemes, or by
+    /// [embedding media queries in the SVG](https://timkadlec.com/2013/04/media-queries-within-svg/).
+    ///
+    /// For a "square" symbol, one can use the URL-escaped version of
+    /// `data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg'><rect width='50' height='50' /></svg>`.
+    pub icon: Option<String>,
+
+    /// Centralized gateway known to have reliable access to decentralized storage
+    /// assets referenced by `reference` or `media` URLs.
+    ///
+    /// Can be used by other frontends for initial retrieval of assets, even if
+    /// these frontends then replicate the data to their own decentralized nodes,
+    /// which they are encouraged to do.
+    pub base_uri: Option<String>,
+
+    /// A link to a valid JSON file containing various keys offering supplementary
+    /// details on the token.  
+    ///
+    /// Eg:
+    /// - `/ipfs/QmdmQXB2mzChmMeKY47C43LxUdg1NDJ5MWcKMKxDu7RgQm`.
+    /// - `https://example.com/token.json`.
+    ///
+    /// If the information given in this document conflicts with the on-chain
+    /// attributes, the values in reference shall be considered the source of truth.
+    pub reference: Option<String>,
+
+    /// The base64-encoded sha256 hash of the JSON file contained in the `reference`
+    /// field. This is to guard against off-chain tampering.
+    pub reference_hash: Option<Base64VecU8>,
+}
+
+pub fn nft_contract_metadata_example() -> NFTContractMetadata {
+    NFTContractMetadata {
+        spec: NFT_METADATA_SPEC.into(),
+        name: "Mochi Rising — Digital Edition".into(),
+        symbol: "MOCHI".into(),
+        icon: Some("data%3Aimage%2Fsvg%2Bxml%2C%3Csvg%20xmlns%3D%27http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%27%3E%3Crect%20width%3D%2750%27%20height%3D%2750%27%20%2F%3E%3C%2Fsvg%3E".into()),
+        base_uri: None,
+        reference: Some("https://example.com/token.json".into()),
+        // from the hex:
+        // 00112233445566778899AABBCCDDEEFF0112233445566778899AABBCCDDEEFF0
+        reference_hash: Some(b"ABEiM0RVZneImaq7zN3u/wESIzRFVmd4iZqrvM3e7/A=".to_vec().into()),
+    }
 }
 
 /// Metadata on the individual token level.
+///
+/// Based on the standard Non-Fungible Token Metadata.
+///
+/// See [NEP-177](https://nomicon.io/Standards/Tokens/NonFungibleToken/Metadata) for
+/// more info.
+#[cfg_attr(not(target_arch = "wasm32"), derive())]
 #[derive(
-    Debug, Clone, Serialize, Deserialize, PartialEq, BorshDeserialize, BorshSerialize, JsonSchema,
+    Clone, Debug, PartialEq, Serialize, Deserialize, BorshDeserialize, BorshSerialize, JsonSchema,
 )]
 #[serde(crate = "near_sdk::serde")]
+#[serde(deny_unknown_fields)]
 #[schemars(crate = "near_sdk::schemars")]
+#[schemars(example = "token_metadata_example")]
 pub struct TokenMetadata {
-    pub title: Option<String>, // ex. "Arch Nemesis: Mail Carrier" or "Parcel #5055"
-    pub description: Option<String>, // free-form description
-    pub media: Option<String>, // URL to associated media, preferably to decentralized, content-addressed storage
-    pub media_hash: Option<Base64VecU8>, // Base64-encoded sha256 hash of content referenced by the `media` field. Required if `media` is included.
-    pub copies: Option<u64>, // number of copies of this set of metadata in existence when token was minted.
-    pub issued_at: Option<String>, // ISO 8601 datetime when token was issued or minted
-    pub expires_at: Option<String>, // ISO 8601 datetime when token expires
-    pub starts_at: Option<String>, // ISO 8601 datetime when token starts being valid
-    pub updated_at: Option<String>, // ISO 8601 datetime when token was last updated
-    pub extra: Option<String>, // anything extra the NFT wants to store on-chain. Can be stringified JSON.
-    pub reference: Option<String>, // URL to an off-chain JSON file with more info.
-    pub reference_hash: Option<Base64VecU8>, // Base64-encoded sha256 hash of JSON from reference field. Required if `reference` is included.
+    /// The name of this specific token.
+    ///
+    /// Eg. "Arch Nemesis: Mail Carrier" or "Parcel #5055"
+    pub title: Option<String>,
+
+    /// Free-form description of the token.
+    pub description: Option<String>,
+
+    /// URL to associated media.
+    ///
+    /// Preferably to decentralized, content-addressed storage.
+    pub media: Option<String>,
+
+    /// Base64-encoded sha256 hash of content referenced by the `media` field.
+    ///
+    /// This is to guard against off-chain tampering.
+    ///
+    /// Required if `media` is included.
+    pub media_hash: Option<Base64VecU8>,
+
+    /// The number of tokens with this set of metadata or `media` known to exist at
+    /// time of minting.
+    pub copies: Option<u64>,
+
+    /// Unix epoch in milliseconds when token was issued or minted (an unsigned
+    /// 32-bit integer would suffice until the year 2106).
+    pub issued_at: Option<String>,
+
+    /// Unix epoch in milliseconds when token expires.
+    pub expires_at: Option<String>,
+
+    /// Unix epoch in milliseconds when token starts being valid.
+    pub starts_at: Option<String>,
+
+    /// Unix epoch in milliseconds when token was last updated.
+    pub updated_at: Option<String>,
+
+    /// Anything extra the NFT wants to store on-chain. Can be stringified JSON.
+    pub extra: Option<String>,
+
+    /// URL to an off-chain JSON file with more info.
+    pub reference: Option<String>,
+
+    /// Base64-encoded sha256 hash of JSON from `reference` field. Required if reference is included.
+    pub reference_hash: Option<Base64VecU8>,
+}
+
+pub fn token_metadata_example() -> TokenMetadata {
+    TokenMetadata {
+        title: Some("Arch Nemesis: Mail Carrier".into()),
+        description: Some("My free-form description".into()),
+        media: Some("https://example.com/token/media.xyz".into()),
+        // from the hex:
+        // 00112233445566778899AABBCCDDEEFF0112233445566778899AABBCCDDEEFF0
+        media_hash: Some(b"ABEiM0RVZneImaq7zN3u/wESIzRFVmd4iZqrvM3e7/A=".to_vec().into()),
+        copies: Some(1),
+        issued_at: Some("1640995200000".into()),
+        expires_at: Some("1640995200000".into()),
+        starts_at: Some("1640995200000".into()),
+        updated_at: Some("1640995200000".into()),
+        extra: Some(r#"{"my_key_a": false, "my_key_b": {"inner": [1, 2, 3]}}"#.into()),
+        reference: Some("https://example.com/token.json".into()),
+        // from the hex:
+        // 00112233445566778899AABBCCDDEEFF0112233445566778899AABBCCDDEEFF0
+        reference_hash: Some(b"ABEiM0RVZneImaq7zN3u/wESIzRFVmd4iZqrvM3e7/A=".to_vec().into()),
+    }
 }
 
 /// Offers details on the contract-level metadata.
